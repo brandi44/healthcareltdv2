2cc1f3ea9ee8cfad6c4192224efbd82cac8637a0
brandi44 <42127840+brandi44@users.noreply.github.com>
2018-10-11 10:16:46 +1100

    jenkins, pom.

---
+++ src/com/luv2code/jsf/jdbc/jenkins_demo.java
@@ -0,0 +1,62 @@
+package com.luv2code.jsf.jdbc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class jenkins_demo {
+/**
+* @param args
+*/
+       public static void main(String[] args) {
+    	   String exePath = "C:\\Users\\brand\\Desktop\\SEP\\chromedriver.exe";
+    	   System.setProperty("webdriver.chrome.driver", exePath);
+    	   
+// objects and variables instantiation
+              WebDriver driver = new ChromeDriver();
+              String appUrl = "http://localhost:8081/Healthcareltd/faces/login.xhtml";
+             
+// launch the chrome browser and open the application url
+              driver.get(appUrl);
+             
+// maximize the browser window
+              driver.manage().window().maximize();
+             
+// declare and initialize the variable to store the expected title of the webpage.
+              String expectedTitle = "Healthcareltd";
+             
+// fetch the title of the web page and save it into a string variable
+              String actualTitle = driver.getTitle();
+             
+// compare the expected title of the page with the actual title of the page and print the result
+              if (expectedTitle.equals(actualTitle))
+              {
+                     System.out.println("Verification Successful - The correct title is displayed on the web page.");
+              }
+             else
+              {
+                     System.out.println("Verification Failed - An incorrect title is displayed on the web page.");
+              }
+
+// enter a valid username in the email textbox
+              WebElement username = driver.findElement(By.id("form:username"));
+              username.clear();
+              username.sendKeys("TestSelenium");
+             
+// enter a valid password in the password textbox
+              WebElement password = driver.findElement(By.id("form:password"));
+              password.clear();
+              password.sendKeys("password123");
+             
+// click on the Sign in button
+              WebElement SignInButton = driver.findElement(By.id("form:submit"));
+              SignInButton.click();
+             
+// close the web browser
+              driver.close();
+              System.out.println("Test script executed successfully.");
+             
+// terminate the program
+              System.exit(0);
+       }
+}
